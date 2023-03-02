cc6d039a78d02ec6187ebcda9b7c6f303d9d0bc7
z4okolatka <78019576+z4okolatka@users.noreply.github.com>
2023-03-02 16:32:24 +0400

    main

---
+++ Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _init.DisableShip();
+        }
+    }
+}
